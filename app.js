1938ac366ec6654baa6dc8eec3c9d67e1ab2e3b4
ahsaza <ahsaza@protonmail.com>
2021-03-23 00:13:10 +0200

    Adding the js logic to the project

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT | 5000;
 
+// Middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
 // Import Route
 const weatherRoute = require("./routes/weather");
 
@@ -10,6 +14,5 @@ app.set('view engine', 'ejs');
 
 // Middleware route
 app.use("/", weatherRoute);
-app.use(express.static("public"));
 
 app.listen(PORT, () => console.log(`Server starting at port ${PORT}`));
